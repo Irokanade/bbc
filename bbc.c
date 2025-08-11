5e3b933d5ea8ba52ba6464485c9e24bf3ddb738c
Irokanade <michaeljs.leong@gmail.com>
2025-08-12 01:16:29 +0800

    feat: added magic number candidate

--- bbc.c
+++ bbc.c
@@ -46,6 +46,57 @@ const char *square_to_coordinates[] = {
 /**********************************\
  ==================================
  
+           Random numbers
+ 
+ ==================================
+\**********************************/
+
+// pseudo random number state
+unsigned int state = 1804289383;
+
+// generate 32-bit pseudo legal numbers
+unsigned int get_random_U32_number() {
+    // get current state
+    unsigned int number = state;
+    
+    // XOR shift algorithm
+    number ^= number << 13;
+    number ^= number >> 17;
+    number ^= number << 5;
+    
+    // update random number state
+    state = number;
+    
+    // return random number
+    return number;
+}
+
+// generate 64-bit pseudo legal numbers
+U64 get_random_U64_number() {
+    // define 4 random numbers
+    U64 n1;
+    U64 n2;
+    U64 n3;
+    U64 n4;
+    
+    // init random numbers slicing 16 bits from MS1B side
+    n1 = (U64)(get_random_U32_number() & 0xFFFF);
+    n2 = (U64)(get_random_U32_number() & 0xFFFF);
+    n3 = (U64)(get_random_U32_number() & 0xFFFF);
+    n4 = (U64)(get_random_U32_number() & 0xFFFF);
+    
+    // return random number
+    return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
+}
+
+// generate magic number candidate
+U64 generate_magic_number() {
+    return get_random_U64_number() & get_random_U64_number() & get_random_U64_number();
+}
+
+/**********************************\
+ ==================================
+ 
           Bit manipulations
  
  ==================================
@@ -525,31 +576,14 @@ U64 set_occupancy(int index, int bits_in_mask, U64 attack_mask) {
  ==================================
 \**********************************/
 
-// pseudo random number state
-unsigned int state = 1804289383;
-
-// generate 32-bit pseudo legal numbers
-unsigned int get_random_number() {
-    // get current state
-    unsigned int number = state;
-    
-    // XOR shift algorithm
-    number ^= number << 13;
-    number ^= number >> 17;
-    number ^= number << 5;
-    
-    // update random number state
-    state = number;
-    
-    // return random number
-    return number;
-}
-
 int main() {
     // init leaper pieces attacks
     init_leapers_attacks();
 
-    printf("%ud\n", get_random_number());
+    print_bitboard((U64)get_random_U32_number());
+    print_bitboard((U64)(get_random_U32_number() & 0xFFFF)); // slice upper (from MS1b side) 16 bits
+    print_bitboard(get_random_U64_number());
+    print_bitboard(generate_magic_number());
     
     return 0;
 }
