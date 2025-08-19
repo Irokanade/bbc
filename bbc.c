87b049abc572e3e937b496c3371988c6d069443f
Irokanade <michaeljs.leong@gmail.com>
2025-08-19 23:26:54 +0800

    feat: added make move function

--- bbc.c
+++ bbc.c
@@ -1480,6 +1480,42 @@ void print_move_list(moves *move_list) {
     memcpy(occupancies, occupancies_copy, 24);                            \
     side = side_copy, enpassant = enpassant_copy, castle = castle_copy;   \
 
+// move_types
+enum { all_moves, only_captures };
+
+// make move on chess board
+static inline int make_move(int move, int move_flag) {
+    // quite moves
+    if (move_flag == all_moves) {
+        // preserve board state
+        copy_board();
+        
+        // parse move
+        int source_square = get_move_source(move);
+        int target_square = get_move_target(move);
+        int piece = get_move_piece(move);
+        int promoted = get_move_promoted(move);
+        int capture = get_move_capture(move);
+        int double_push = get_move_double(move);
+        int enpass = get_move_enpassant(move);
+        int castling = get_move_castling(move);
+        
+        // move piece
+        pop_bit(bitboards[piece], source_square);
+        set_bit(bitboards[piece], target_square);
+    } else {
+        // capture moves
+        // make sure move is the capture
+        if (get_move_capture(move)) {
+            make_move(move, all_moves);
+        } else {
+            // otherwise the move is not a capture
+            // don't make it
+            return 0;
+        }       
+    }
+}
+
 // generate all moves
 static inline void generate_moves(moves *move_list) {
     // init move count
@@ -1894,26 +1930,30 @@ int main() {
     parse_fen(tricky_position);
     print_board();
     
-    // create move list
+    // create move list instance
     moves move_list;
-        
+    
     // generate moves
     generate_moves(&move_list);
     
-    // print move list
-    print_move_list(&move_list);
-
-    // preserve board state
-    copy_board();
-    
-    // parse fen
-    parse_fen(empty_board);
-    print_board();
-    
-    // restore board state
-    take_back();
-
-    print_board();
+    // loop over generated moves
+    for (int move_count = 0; move_count < move_list.count; ++move_count) {
+        // init move
+        int move = move_list.moves[move_count];
+        
+        // preserve board state
+        copy_board();
+        
+        // make move
+        make_move(move, all_moves);
+        print_board();
+        getchar();
+        
+        // take back
+        take_back();
+        print_board();
+        getchar();
+    }
     
     return 0;
 }
